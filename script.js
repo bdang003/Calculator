e461388ad2439ac94898a12e3bec78fb6bf6bc5d
Brian <bdang0003@gmail.com>
2021-05-21 17:06:01 -0700

    added updateDisplay() and have functioning buttonInput()

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function operate(operator, a, b){
 
 }
+
+function updateDisplay(){
+    display.innerHTML=inputValue;
+}
+
+function enterNumber(){
+    numberInputted = Number(this.innerHTML);
+    inputValue = inputValue * 10 + numberInputted;
+    updateDisplay();    
+}
+
+let inputValue = 0;
+const display = document.querySelector('#display');
+const numbers = Array.from(document.querySelectorAll('.number'));
+numbers.forEach(element => element.addEventListener('click', enterNumber));
+
