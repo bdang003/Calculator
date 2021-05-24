5201af7cd6517fd4c7c807f4d62d01108e2773a3
Brian <bdang0003@gmail.com>
2021-05-24 11:55:50 -0700

    added deimcal functionality: issue with floating numbers

--- script.js
+++ script.js
@@ -3,16 +3,31 @@ function operate(operator, a, b){
 }
 
 function updateDisplay(){
-    display.innerHTML=inputValue;
+    display.innerHTML=currentInputValue;
 }
 
 function enterNumber(){
-    numberInputted = Number(this.innerHTML);
-    inputValue = inputValue * 10 + numberInputted;
+    numberInputted = +this.innerHTML;
+
+    console.log(`inputtedNumber + 10 * decimal index: ${numberInputted / (10*decimalIndex)}`);
+    
+    if(!containDecimal){
+        currentInputValue = currentInputValue * 10 + numberInputted;
+    }
+    else{
+        currentInputValue = currentInputValue + (numberInputted / (10*decimalIndex));
+        decimalIndex*=10;;
+    }
+
     updateDisplay();    
 }
 
-let inputValue = 0;
+let currentInputValue = 0;
+let storedInputValue = 0; 
+let operator = '';
+let containDecimal = true;
+let decimalIndex = 1; //enabled user to input decimal
+
 const display = document.querySelector('#display');
 const numbers = Array.from(document.querySelectorAll('.number'));
 numbers.forEach(element => element.addEventListener('click', enterNumber));
